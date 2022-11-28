11aad8a095240262518219429a03943d817163ec
Nils21n <118370915+Nils21n@users.noreply.github.com>
2022-11-28 21:46:55 +0100

    Create Bat2022.h

---
+++ Bat2022.h
@@ -0,0 +1,90 @@
+//
+// Created by Nils on 28/11/2022.
+//
+
+#ifndef BAT2022_BAT2022_H
+#define BAT2022_BAT2022_H
+
+void grille(int tab[15][15])
+{
+    printf("   1  2  3  4  5  6  7  8  9  10 11 12 13 14 15");
+    printf("\n");
+
+    for (char j='A';j<'K';j++)
+    {
+        printf(" %c",j);
+        printf(" X  X  X  X  X  X  X  X  X  X  X  X  X  X  X");
+        printf("\n" );
+    }
+}
+
+int menu()
+{
+    int choix=0;
+    while(choix<1 || choix>6)
+    {
+        printf("Que voulez-vous faire: \n");
+        printf("1 :Jouer\n");
+        printf("2 :Charger\n");
+        printf("3 :Aide\n");
+        printf("4 :Quitter\n");
+        printf("5 :Sauvegarder\n");
+        printf("Votre choix ? ");
+        scanf("%d", &choix);
+    }
+    return choix;
+}
+
+void SaisirL()
+{
+    int i;
+    do
+    {
+        printf("Choissisez la ligne: ");
+        scanf("%d",&i);
+    }while(i<0 ||i>15)
+        return i;
+
+}
+
+void SaisirC()
+{
+    int i;
+    do
+    {
+        printf("Choissisez la colone: ");
+        scanf("%d",&i);
+        printf("\n");
+    }while(i<0 ||i>15)
+    return i;
+}
+
+int saisirOrien()
+{
+    int i;
+    do
+    {
+       printf("Choissisez une orientation \n0-) p")
+    }
+}
+
+void Charger()
+{
+
+}
+
+void Aide()
+{
+
+}
+
+void Quitter()
+{
+
+}
+
+void Sauvegarder()
+{
+
+}
+#endif //BAT2022_BAT2022_H
